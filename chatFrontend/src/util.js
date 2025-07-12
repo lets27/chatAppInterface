4efe5a81d6eaf533d14d322914f62b134795cb3e
LETSO27 <letsomogwera61@gmail.com>
2025-07-12 14:11:06 +0200

    updates baseurl

--- chatFrontend/src/util.js
+++ chatFrontend/src/util.js
@@ -1,2 +1,4 @@
 export const baseUrl =
-  import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000" // No /api in dev
+    : ""; // Empty in production
